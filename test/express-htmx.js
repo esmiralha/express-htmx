0e1635c4123a95ae44c1ea6f53d9ac509b19d3d3
Luiz Esmiralha <>
2023-01-02 17:35:24 -0300

    tests

--- test/express-htmx.js
+++ test/express-htmx.js
@@ -1,3 +1,25 @@
-const {describe} = require("mocha");
+const { describe, test } = require("mocha");
+const { assert } = require("chai");
+const htmx = require("../index");
 
-describe("");
+describe("htmx request header", () => {
+    test("abssent", () => {
+        const htmxMiddleware = htmx.middleware;
+        const req = { get: (header) => { return undefined; }};
+        const res = {};
+        htmxMiddleware(req, res, () => { });
+        assert.isFalse(req.htmx.isHtmx);
+    });
+    test("absent", () => {
+        const htmxMiddleware = htmx.middleware;
+        const req = {
+            get: (header) => {
+                const headers = {"HX-Request": "true"};
+                return headers[header];
+            }
+        };
+        const res = {};
+        htmxMiddleware(req, res, () => { });
+        assert.isTrue(req.htmx.isHtmx);
+    });
+});
